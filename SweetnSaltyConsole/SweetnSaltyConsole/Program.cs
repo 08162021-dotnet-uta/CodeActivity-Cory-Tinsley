e9b86a9ebdaf8e5ae32730780c307fe3712de982
Cory Tinsley <cory.tinsley@revature.net>
2021-09-15 01:11:22 -0500

    removing accidental push

---
+++ SweetnSaltyConsole/SweetnSaltyConsole/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace SweetnSaltyConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintNumbers();
+        }
+
+        static void PrintNumbers()
+        {
+            int sweet = 0;
+            int salty = 0;
+            int sweetnSalty = 0;
+            for(int i = 1; i <= 1000; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.Write("Sweet'nSalty ");
+                    sweetnSalty++;
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.Write("Sweet ");
+                    sweet++;
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.Write("Salty ");
+                    salty++;
+                }
+                else
+                {
+                    Console.Write(i + " ");
+                }
+
+                if (i % 10 == 0)
+                    Console.Write("\n");
+                
+
+            }
+
+            Console.WriteLine($"There were {sweet} Sweets.");
+            Console.WriteLine($"There were {salty} Salty's.");
+            Console.WriteLine($"There were {sweetnSalty} Sweet'nSalty.");
+
+        }
+    }//End of Program
+}//End of Class
